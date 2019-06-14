ba3f9c6179cad15d6ac2fac352aebda5965a2558
NinKorn <NinKorn@163.com>
2019-06-14 18:17:04 +0800

    创建动画组件

---
+++ src/Loading.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+// 加载动画
+class Loading extends Component {
+    state = {
+        
+    }
+    render() {
+        return (
+            <div className="loading"> 
+               
+            </div>
+        )
+    }
+}
+
+
+export default Loading;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HelloWord from './helloWorld';
+import HelloWord from './HelloWorld';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<HelloWord name1='react' name2='vue'/>, document.getElementById('root'));
