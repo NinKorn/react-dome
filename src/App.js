be64920d6259159c4be59583112cb8b0e2e30270
NinKorn <NinKorn@163.com>
2019-06-18 16:46:50 +0800

    react-route 模块化

--- src/App.js
+++ src/App.js
@@ -17,26 +17,29 @@
 
 // --- 父子组件传值
 import React, { Component } from 'react';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import routes from './assets/router';
 import Button from './Button';
 import Dialog from './Dialog';
-import News from './News';
+// import Home from './Home';
+// import News from './News';
 import './style.css';
- 
+
 class App extends Component {
     state = {
         loading: false,
         dialog: false,
         message: 'xxx'
     }
- 
+
     submit = () => {
         this.setState({
             loading: true
         })
- 
+
         // 模拟数据请求的过程，假设数据请求会经历1s得到结果
         setTimeout(() => {
- 
+
             // 通过随机数的方式模拟可能出现的成功与失败两种结果
             const res = Math.random(1);
             if (res > 0.5) {
@@ -53,26 +56,42 @@ class App extends Component {
             this.setState({ loading: false })
         }, 1000)
     }
- 
+
     close = () => {
         this.setState({
             dialog: false
         })
     }
- 
-    render () {
- 
+
+    render() {
+
         const { loading, dialog, message } = this.state;
- 
+
         return (
             <div className="app-wrap">
-                <Button loadingButton={ loading } submitButton={ this.submit }>提交</Button>
-                { dialog && <Dialog message={ message } close={ this.close } /> }
-                <News></News>
+                <Button loadingButton={loading} submitButton={this.submit}>提交</Button>
+                {dialog && <Dialog message={message} close={this.close} />}
+                {/* <News></News> */}
+                <Router>
+                    <div>
+                        <Switch>
+                            {/* //主要逻辑在这里 */}
+                            {
+                                routes.map((item, i) => {
+                                    if (item.exact) {
+                                        return <Route exact path={item.path} component={item.component} key={i} />
+                                    } else {
+                                        return <Route path={item.path} component={item.component} key={i} />
+                                    }
+                                })
+                            }
+                        </Switch>
+                    </div>
+                </Router>
             </div>
         )
     }
 }
- 
+
 export default App;
 // --- 父子组件传值
